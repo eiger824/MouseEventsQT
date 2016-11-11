770aa877d327c6f29a13980338e641bffc21a59d
eiger824 <santiago.pagola@alten.se>
2016-11-11 15:52:50 +0100

    modified label text

--- gui.cpp
+++ gui.cpp
@@ -6,7 +6,8 @@
 Gui::Gui(QWidget* parent) : QWidget(parent) {
 	setFixedSize(300,300);
 	QVBoxLayout *main = new QVBoxLayout;
-	QLabel* dummy = new QLabel("Hello");
+	QLabel* dummy = new QLabel("Press and release the mouse wherever you want and see what happens in the terminal...");
+	dummy->setWordWrap(true);
 	main->addWidget(dummy);
 	setLayout(main);
 	show();
